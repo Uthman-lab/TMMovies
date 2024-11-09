6291d39d5d25b6697d2ccf91bd9717e7791acbf2
AsumahBanda <117347715+AsumahBanda@users.noreply.github.com>
2024-11-09 06:20:14 +0000

    setting up project

---
+++ TMMovies/TMMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMMoviesApp.swift
+//  TMMovies
+//
+//  Created by AMALITECH-PC-593 on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TMMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
